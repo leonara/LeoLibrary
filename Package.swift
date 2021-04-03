cd0df0d02ef4b2673d3f17d587a5460ec9ae13a7
Leonara Martins <leonara.martins@farfetch.com>
2021-04-03 09:50:32 -0300

    fourth commit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LeoLibrary",
-            targets: ["LeoLibrary","LeoLibraryBinary"]),
+//            targets: ["LeoLibrary","LeoLibraryBinary"]),
+        targets: ["LeoLibraryBinary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,10 +19,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LeoLibrary",
-            dependencies: [],
-            path: "Source"),
+//        .target(
+//            name: "LeoLibrary",
+//            dependencies: [],
+//            path: "Source"),
         .binaryTarget(
             name: "LeoLibraryBinary",
             path: "binary/LeoLibrary.xcframework"),
