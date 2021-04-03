134561920792e97aa8f5aa89ae7bb309de03f068
Leonara Martins <leonara.martins@farfetch.com>
2021-04-03 10:19:07 -0300

    outro xcframework

---
+++ binary/LeoLibrary.xcframework/ios-arm64/LeoLibrary.framework/Headers/LeoLibrary.h
@@ -0,0 +1,18 @@
+//
+//  LeoLibrary.h
+//  LeoLibrary
+//
+//  Created by Leonara Martins on 03/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeoLibrary.
+FOUNDATION_EXPORT double LeoLibraryVersionNumber;
+
+//! Project version string for LeoLibrary.
+FOUNDATION_EXPORT const unsigned char LeoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeoLibrary/PublicHeader.h>
+
+
